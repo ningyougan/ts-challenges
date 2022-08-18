cf10eb9496e6180426c3c6ea482b83801493e85d
Polaris <54269504@qq.com>
2022-08-19 01:31:29 +0800

    update day1

---
+++ type-challenges/easy-pick/templat.ts
@@ -0,0 +1,16 @@
+type MyPick<T, K extends keyof T> = {
+  [P in K] : T [P]
+}
+
+function MyPick(todo, keys){
+
+  const obj = {}
+
+  keys.forEach(key => {
+    if(key in todo){
+      obj[key] = todo[key];
+    }
+  });
+
+  return obj;
+}
