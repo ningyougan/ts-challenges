02d0239241fae17c874a60bfc50df38a5d710bbb
Polaris <54269504@qq.com>
2022-08-20 22:16:34 +0800

    tuple-to-object

---
+++ README.md
@@ -0,0 +1,16 @@
+# setup
+
+```bash
+npm i -g pnpm
+pnpm install
+```
+
+# Docs
+
+www.typescriptlang.org
+
+http://www.patrickzhong.com/TypeScript/PREFACE.html
+
+# github
+
+https://github.com/type-challenges/type-challenges
---
+++ type-challenges/tuple-to-object/template.ts
@@ -0,0 +1,21 @@
+export type TupleToObject<T extends readonly any[]> = {
+  [P in T[number]]: P;
+};
+
+function tupleToObject(array: []) {
+  const obj = {};
+
+  array.forEach((val) => {
+    obj[val] = val;
+  });
+
+  return obj;
+}
+
+// 1. 返回一个对象
+// 2. 遍历一个array T[number]
+// const let js
+// type interface
+
+// 1. typeof
+// 2. 字面量类型
