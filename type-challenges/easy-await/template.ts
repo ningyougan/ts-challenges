1fd8f06f43d2186b0c1eb6361b570226af8df4be
Polaris <54269504@qq.com>
2022-08-25 23:30:47 +0800

    update await

---
+++ type-challenges/easy-await/template.ts
@@ -0,0 +1,9 @@
+export type MyAwaited<T extends Promise<unknown>> = T extends Promise<infer X>
+  ? X extends Promise<unknown>
+    ? MyAwaited<X>
+    : X
+  : never;
+
+// infer
+// 1. 只能在条件类型中使用
+// 2. 设置未知数 变量
